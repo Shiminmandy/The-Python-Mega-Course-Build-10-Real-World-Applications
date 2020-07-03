9179f65fe06ec08a940182501dab5f70f52b688d
Shiminmandy <mandyshimin18@gmail.com>
2020-07-02 22:55:13 -0400

    openfile

--- docs/03_python_basics.py
+++ docs/03_python_basics.py
@@ -3,3 +3,27 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+"""
+16. Reading Text From a File
+"""
+myfile = open("subjects.txt", 'r')
+# print(myfile.read())  # it will print the content inside the txt
+# if we want to read the file two times
+readfile = myfile.read()
+# close file to delete memory
+myfile.close()
+print(readfile)
+print(readfile)
+
+# second way to read the file
+with open("subjects.txt", 'r') as openfile:
+    content = openfile.read()
+print(content)
+
+"""
+17. Different Filepath
+"""
+# if file is in different path, we will get FileNotFound error
+with open("The_Python_Mega_Course/sdfghj.py") as myfile2:
+    content2 = myfile2.read()
+print(content2)
