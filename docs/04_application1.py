939308e6366a68c8269220162c4d207a15586942
Shiminmandy <mandyshimin18@gmail.com>
2020-07-05 15:17:26 -0400

    simple translator

--- docs/04_application1.py
+++ docs/04_application1.py
@@ -6,5 +6,21 @@
 import json
 
 data = json.load(open("file/data.json"))
-print(type(data))   # <class 'dict'>  dictionary type can help us find word and definition conveniently
+# print(type(data))   # <class 'dict'>  dictionary type can help us find word and definition conveniently
 # print(data)       # we will get a dictionary which contains key words and their definitions
+# test
+# print(data['rain'])
+"""
+['Precipitation in the form of liquid water drops with diameters greater than 0.5 millimetres.',
+ 'To fall from the clouds in drops of water.']
+"""
+
+
+def translate(word):
+    return data[word]
+
+def main():
+    word = input("Enter word: ")
+    print(word)
+if __name__ == '__main__':
+    main()
