d2a6a029bedbd42471b8d352eb2d93330ce2a0ba
Shiminmandy <mandyshimin18@gmail.com>
2020-07-05 13:50:35 -0400

    load dictionary

--- docs/04_application1.py
+++ docs/04_application1.py
@@ -3,3 +3,8 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+import json
+
+data = json.load(open("file/data.json"))
+print(type(data))   # <class 'dict'>  dictionary type can help us find word and definition conveniently
+# print(data)       # we will get a dictionary which contains key words and their definitions
