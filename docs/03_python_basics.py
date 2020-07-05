1f6c92e73b8b1330736867825db55ed95008912d
Shiminmandy <mandyshimin18@gmail.com>
2020-07-04 23:15:25 -0400

    modules

--- docs/03_python_basics.py
+++ docs/03_python_basics.py
@@ -54,3 +54,43 @@ print(get_content)
 20. Modules
 """
 # import + modules' name
+# get a list of modules
+import sys
+import time
+
+print(sys.builtin_module_names)
+# ('_abc', '_ast', '_bisect', '_blake2', '_codecs', '_codecs_cn', '_codecs_hk', '_codecs_iso2022', '_codecs_jp',)
+# ('_codecs_kr', '_codecs_tw', '_collections', '_contextvars', '_csv', '_datetime', '_functools', '_heapq', '_imp',)
+# ('_io', '_json', '_locale', '_lsprof', '_md5', '_multibytecodec', '_opcode', '_operator', '_pickle', '_random', )
+# ('_sha1', '_sha256', '_sha3', '_sha512', '_signal', '_sre', '_stat', '_statistics', '_string', '_struct', )
+# ('_symtable', '_thread', '_tracemalloc', '_warnings', '_weakref', '_winapi', '_xxsubinterpreters', 'array', 'atexit',)
+# ('audioop', 'binascii', 'builtins', 'cmath', 'errno', 'faulthandler', 'gc', 'itertools', 'marshal', 'math', 'mmap', )
+# ('msvcrt', 'nt', 'parser', 'sys', 'time', 'winreg', 'xxsubtype', 'zlib')
+# these were written in C language inside the python software
+
+# standard python modules
+import os
+
+# while True:
+#     if os.path.exists("file/vegetables.txt"):
+#         with open("file/vegetables.txt") as file:
+#             print(file.read())
+#     else:
+#         print("File does not exist.")
+#     time.sleep(3)  # because the file does not created, we will get File does not exist every 3 seconds
+
+# third-party modules
+# pip is used to install other third-party libraries
+import pandas
+
+while True:
+    if os.path.exists("file/temps_today.csv"):
+        data = pandas.read_csv("file/temps_today.csv")
+        print(data.mean())
+        print(data.mean()["st1"])  # 22.15
+    else:
+        print("File does not exist.")
+    time.sleep(5)
+# st1    22.15
+# st2    20.55
+# dtype: float64
