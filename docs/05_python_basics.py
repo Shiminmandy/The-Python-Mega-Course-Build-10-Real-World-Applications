f588b046786dae604673cee47d00ad24b9bb32f2
Shiminmandy <mandyshimin18@gmail.com>
2020-07-08 00:23:35 -0400

    start pandas

--- docs/05_python_basics.py
+++ docs/05_python_basics.py
@@ -1,14 +1,53 @@
 # -*- coding:utf-8 -*-
-# @Description
+# @Description: learn to use pandas
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
-import mysql.connector
+import pandas
 
-con = mysql.connector.connect(
-    user="shimin",
-    password="whatever",
-    host="108.167.124.172",
-    database="shimin_pmldatabase"
-)
-cursor = con.cursor()
+# pass lists
+df1 = pandas.DataFrame([[10, 20, 30], [40, 50, 60], [70, 80, 90]], columns=['Price', 'Age', 'Value'],
+                       index=['First', 'Second', 'Third'])
+print(df1)
+"""
+        Price  Age  Value
+First      10   20     30
+Second     40   50     60
+Third      70   80     90
+"""
+# pass dictionaries
+df2 = pandas.DataFrame([{'Name': ['Shimin', 'Alex']}, {'surName': 'Jack'}])
+print(df2)
+# [{'Name': ['Shimin', 'Alex']}, {'Name': 'Jack'}]
+"""
+             Name
+0  [Shimin, Alex]
+1            Jack
+"""
+# [{'Name': ['Shimin', 'Alex']}, {'surName': 'Jack'}]
+"""
+             Name surName
+0  [Shimin, Alex]     NaN
+1             NaN    Jack
+"""
+print(type(df2))
+# <class 'pandas.core.frame.DataFrame'>
+# print(dir(df2)) we will get a list of method of pandas
+print(df1.mean())
+"""
+Price    40.0
+Age      50.0
+Value    60.0
+dtype: float64
+
+"""
+print(type(df1.mean()))
+# <class 'pandas.core.series.Series'>
+print(df1.Price)
+"""
+First     10
+Second    40
+Third     70
+"""
+print(df1.Price.mean())
+# 40.0
--- docs/file/source.py
+++ docs/file/source.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 con = mysql.connector.connect(
 user = "ardit700_student",
 password = "ardit700_student",
@@ -8,19 +9,21 @@ database = "ardit700_pm1database"
 )
 
 cursor = con.cursor()
+# add while loop
+while True:
 
-word=input("Enter the word: ")
+    word = input("Enter the word: ")
 
-query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
-results = cursor.fetchall()
+    query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
+    results = cursor.fetchall()
 
-# print(results) word = rain     # we will get a list of definitions in side tuples
-# [('Precipitation in the form of liquid water drops with diameters greater than 0.5 millimetres.',),
-# ('To fall from the clouds in drops of water.',)]
-# tuple_list = [('apple', 'banana', 'orange'), ('people')]
-# print(tuple_list[1])      # people
-if results:
-    for result in results:
-        print(result[0])
-else:
-    print("No word found!")
+    # print(results) word = rain     # we will get a list of definitions in side tuples
+    # [('Precipitation in the form of liquid water drops with diameters greater than 0.5 millimetres.',),
+    # ('To fall from the clouds in drops of water.',)]
+    # tuple_list = [('apple', 'banana', 'orange'), ('people')]
+    # print(tuple_list[1])      # people
+    if results:
+        for result in results:
+            print(result[0])
+    else:
+        print("No word found!")
