8c0561b105e51f33d284d076eee2b9124418382d
Shiminmandy <mandyshimin18@gmail.com>
2020-07-07 23:01:25 -0400

    sourse

--- docs/05_python_basics.py
+++ docs/05_python_basics.py
@@ -4,3 +4,11 @@
 # @Copyright
 # @Version:0.0.1
 import mysql.connector
+
+con = mysql.connector.connect(
+    user="shimin",
+    password="whatever",
+    host="108.167.124.172",
+    database="shimin_pmldatabase"
+)
+cursor = con.cursor()
