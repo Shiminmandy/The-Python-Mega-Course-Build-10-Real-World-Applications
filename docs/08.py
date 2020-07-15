00cac4750176951d0581d96e67999c59ccb4d4bb
Shiminmandy <mandyshimin18@gmail.com>
2020-07-14 21:43:12 -0400

    adding points from files

--- docs/08.py
+++ docs/08.py
@@ -11,7 +11,22 @@ fg = folium.FeatureGroup(name='My Map')
 fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 # add multiple points
 fg.add_child(folium.Marker(location=[35.2, -90.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-map1.add_child(fg)
-map1.save("Map1.html")  # open in browser , do not open in pycharm
+# map1.add_child(fg)
+# map1.save("Map1.html")  # open in browser , do not open in pycharm
+# adding points from files/use for loop
 get_data = pandas.read_csv("Volcanoes.txt")
 print(get_data)
+print(type(get_data))       # <class 'pandas.core.frame.DataFrame'>
+print(get_data.columns)
+"""
+Index(['VOLCANX020', 'NUMBER', 'NAME', 'LOCATION', 'STATUS', 'ELEV', 'TYPE',
+       'TIMEFRAME', 'LAT', 'LON'],
+      dtype='object')
+"""
+lat = list(get_data["LAT"])
+# print(lat)   get a list of lat
+lon = list(get_data["LON"])
+for lt, ln in zip(lat, lon):    # for a, b in zip([1,2,3],[4,5,6]), print(a,b) will get 1 4/2 5/ 3 6
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+map1.add_child(fg)
+map1.save("Map1.html")
