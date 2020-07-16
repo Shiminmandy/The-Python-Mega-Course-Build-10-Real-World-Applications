1fe548eae0793f41f2cfe7cb7875a3a82f7ac961
Shiminmandy <mandyshimin18@gmail.com>
2020-07-16 15:36:39 -0400

    app2

--- docs/08.py
+++ docs/08.py
@@ -7,10 +7,10 @@ import folium
 import pandas
 
 map1 = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name='My Map')
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fgv = folium.FeatureGroup(name='Volcanoes')
+fgv.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 # add multiple points
-fg.add_child(folium.Marker(location=[35.2, -90.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+fgv.add_child(folium.Marker(location=[35.2, -90.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 # map1.add_child(fg)
 # map1.save("Map1.html")  # open in browser , do not open in pycharm
 # adding points from files/use for loop
@@ -48,13 +48,19 @@ for lt, ln, el, name in zip(lat, lon, elev, name):  # for a, b in zip([1,2,3],[4
     # fg.add_child(folium.Marker(location=[lt, ln], radius=10, popup=name + '\t' + str(el) + 'm', icon=folium.Icon(
     # color=color_producer(el))))
     # change marker to circle
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=name + '\t' + str(el) + 'm',
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=name + '\t' + str(el) + 'm',
                                      fill_color=color_producer(el), color='gray', fill_opacity=0.7))
 # adding a GeoJson Polygon layer
-fg.add_child(folium.GeoJson(data=(open('file/world.json', 'r', encoding='utf-8-sig').read())))
+# fgp.add_child(folium.GeoJson(data=(open('file/world.json', 'r', encoding='utf-8-sig').read())))
+fgp = folium.FeatureGroup(name='Population')
+fgp.add_child(folium.GeoJson(data=open('file/world.json', 'r', encoding='utf-8-sig').read(),  # don't missing .read()
+                            style_function=lambda x: {'fillColor': 'yellow' if x['properties']['POP2005'] < 10000000
+                            else 'green' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 print(type(el))  # <class 'float'> we need to change to string format in popup window
 
-map1.add_child(fg)
+map1.add_child(fgv)
+map1.add_child(fgp)
+map1.add_child(folium.LayerControl())
 map1.save("Map1.html")
 print(dir(folium))
 # help(folium.CircleMarker)
