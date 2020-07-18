5f3a2fdf8aa942f414860b027555c6f9241ce21a
Shiminmandy <mandyshimin18@gmail.com>
2020-07-17 22:57:15 -0400

    html

--- docs/09_application3/script1.py
+++ docs/09_application3/script1.py
@@ -3,7 +3,7 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
-from flask import Flask
+from flask import Flask, render_template
 
 # 网页浏览器可以读取HTML文件，并将其渲染成可视化网页。
 
@@ -12,12 +12,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Homepage here"   # search http://127.0.0.1:5000/
-
+    # return "Homepage here"   # search http://127.0.0.1:5000/
+    return render_template("home.html")
 
 @app.route('/about/')  # we need to search http://127.0.0.1:5000/about/ instead
 def about():
-    return "Mandy's content goes here"
+    return render_template("about.html")
 
 
 
