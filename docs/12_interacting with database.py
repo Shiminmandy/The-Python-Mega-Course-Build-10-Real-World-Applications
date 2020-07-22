cd57f0838697fc5d7fdd49d448ffc775ff81dbec
Shiminmandy <mandyshimin18@gmail.com>
2020-07-21 22:14:20 -0400

    sqlite

--- docs/12_interacting with database.py
+++ docs/12_interacting with database.py
@@ -4,12 +4,19 @@
 # @Copyright
 # @Version:0.0.1
 import sqlite3
-
+"""
+1. Connect to a database
+2. Create a cursor object
+3.Write an SQL query
+4. Commit changes
+"""
 
 def create_table():
     conn = sqlite3.connect("lite.db")
-    cur = conn.cursor()
+    # if you dont have this file the database will create one for u and the connection will be established
+    cur = conn.cursor()  # cursor object
     cur.execute("CREATE TABLE store (item TXET, quantity INTEGER, price REAL)")
+    # enter SQL code inside the brackets, SQL code always goes inside quotes
     conn.commit()
     conn.close()
 
@@ -17,19 +24,23 @@ def create_table():
 def insert(item, quantity, price):
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
+    # cur.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)") 'Wine Glass' is TEXT, 8 is INTEGER, 10.5 is  REAL
     cur.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
     conn.commit()
     conn.close()
 
 
-insert("Coffee Cup", 10,5)
+insert("Coffee Cup", 10, 5)
+
 
 def view():
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
-    rows=cur.fetchall()
+    rows = cur.fetchall()
     conn.close()
     return rows
 
+
 print(view())
+# [('Coffee Cup', 10, 5.0), ('Coffee Cup', 10, 5.0), ('Coffee Cup', 10, 5.0)]
