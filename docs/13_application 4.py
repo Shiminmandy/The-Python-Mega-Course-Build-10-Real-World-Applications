065c69e1336fe4bc89beef92c8d7b4fb7e81d395
Shiminmandy <mandyshimin18@gmail.com>
2020-07-22 23:53:04 -0400

    updated

---
+++ docs/13_application 4.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# @Description
+# @Author: Shimin
+# @Copyright
+# @Version:0.0.1
+"""
+A prgram that stores this book imformation:
+Title, Author
+Year, ISBN
+
+User can:
+
+view all records
+Search an entry
+Add entry
+Update entry
+Delete
+Close
+"""
