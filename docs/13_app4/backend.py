b0b4bc79d504105b0f2cc0a862b3d2e5620c5a61
Shiminmandy <mandyshimin18@gmail.com>
2020-07-23 21:43:32 -0400

    backend.py

--- docs/13_app4/backend.py
+++ docs/13_app4/backend.py
@@ -3,3 +3,13 @@
 # @Author: Shimin
 # @Copyright
 # @Version:0.0.1
+import sqlite3
+
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS book (id INTEER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
