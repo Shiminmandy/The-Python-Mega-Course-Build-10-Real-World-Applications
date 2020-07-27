9ee943e87504b195a746f986fa51f3cbf2480eb7
Shiminmandy <mandyshimin18@gmail.com>
2020-07-26 23:33:13 -0400

    problematic of updating

--- docs/frontend.py
+++ docs/frontend.py
@@ -1,28 +1,19 @@
-# -*- coding:utf-8 -*-
-# @Description
-# @Author: Shimin
-# @Copyright
-# @Version:0.0.1
 from tkinter import *
 import backend
 
-
-
 def get_selected_row(event):
     global selected_tuple
-    if not list1.curselection():
-        return
-    index=list1.curselection()[0]    # we will get a tuple if we don't add [0]
+    index=list1.curselection()[0]
     selected_tuple=list1.get(index)
-    return(selected_tuple)
     e1.delete(0,END)
     e1.insert(END,selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END, selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END, selected_tuple[3])
-    e4.delete(0, END)
-    e4.insert(END, selected_tuple[4])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
+
 def view_command():
     list1.delete(0,END)
     for row in backend.view():
@@ -33,34 +24,21 @@ def search_command():
     for row in backend.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
         list1.insert(END,row)
 
-
 def add_command():
     backend.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
     list1.delete(0,END)
-    list1.insert(END, (title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
 
 def delete_command():
     backend.delete(selected_tuple[0])
 
-
 def update_command():
-    backend.update(selected_tuple[0], title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
-"""
-A prgram that stores this book imformation:
-Title, Author
-Year, ISBN
-
-User can:
-
-view all records
-Search an entry
-Add entry
-Update entry
-Delete
-Close
-"""
+    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
 
 window=Tk()
+
+window.wm_title("BookStore")
+
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
 
@@ -89,11 +67,9 @@ isbn_text=StringVar()
 e4=Entry(window,textvariable=isbn_text)
 e4.grid(row=1,column=3)
 
-
-list1=Listbox(window,height=6,width=35)
+list1=Listbox(window, height=6,width=35)
 list1.grid(row=2,column=0,rowspan=6,columnspan=2)
 
-# scrollbar method
 sb1=Scrollbar(window)
 sb1.grid(row=2,column=2,rowspan=6)
 
@@ -102,24 +78,22 @@ sb1.configure(command=list1.yview)
 
 list1.bind('<<ListboxSelect>>',get_selected_row)
 
-b1=Button(window,text="View all",width=12,command=view_command)
+b1=Button(window,text="View all", width=12,command=view_command)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="Search entry",width=12,command=search_command)
+b2=Button(window,text="Search entry", width=12,command=search_command)
 b2.grid(row=3,column=3)
 
-b3=Button(window,text="Add entry",width=12,command=add_command)
+b3=Button(window,text="Add entry", width=12,command=add_command)
 b3.grid(row=4,column=3)
 
-b4=Button(window,text="Update selected",width=12,command=update_command)
+b4=Button(window,text="Update selected", width=12,command=update_command)
 b4.grid(row=5,column=3)
 
-b5=Button(window,text="Delete selected",width=12,command=delete_command)
+b5=Button(window,text="Delete selected", width=12,command=delete_command)
 b5.grid(row=6,column=3)
 
-b6=Button(window,text="Close",width=12)
+b6=Button(window,text="Close", width=12,command=window.destroy)
 b6.grid(row=7,column=3)
-
-
 
 window.mainloop()
