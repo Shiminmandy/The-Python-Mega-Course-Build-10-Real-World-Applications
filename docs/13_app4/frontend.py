bab58ef5131af6d7eb765dfcf91a4ad1a7ae85e5
Shiminmandy <mandyshimin18@gmail.com>
2020-07-27 16:09:31 -0400

    need to be improved

--- docs/13_app4/frontend.py
+++ docs/13_app4/frontend.py
@@ -61,6 +61,8 @@ Close
 """
 
 window=Tk()
+
+window.wm_title("BookStore")
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
 
@@ -117,7 +119,7 @@ b4.grid(row=5,column=3)
 b5=Button(window,text="Delete selected",width=12,command=delete_command)
 b5.grid(row=6,column=3)
 
-b6=Button(window,text="Close",width=12)
+b6=Button(window,text="Close",width=12,command=window.destroy)
 b6.grid(row=7,column=3)
 
 
